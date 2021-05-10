a200eff3d6293f85c5c6445fda634853ef851342
rasuloff789 <rasuloff789@gmail.com>
2021-05-10 11:47:37 +0500

    upload get add

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,22 +1,36 @@
+const { ok } = require('assert');
 const authJWT = require('../library/auth.js')
+const fs = require('fs')
+const path = require('path');
 
 module.exports = {
+  GET : function (req,res) {
+    const folderImg = path.dirname(path.dirname(path.dirname(__filename))) + "/data/img";
+    
+    const folderVideo= path.dirname(path.dirname(path.dirname(__filename))) + "/data/video";
+    
+    
+    try {
+      res.send([...fs.readdirSync(folderImg), ...fs.readdirSync(folderVideo)])
+    } catch (error) {
+      res.send(error)
+    }
+  },
   POST: function (req, res) {
-    authJWT(req)
+    // authJWT(req)
     let sampleFile;
     let uploadPath;
-
+    
     const { v4: uuidv4 } = require('uuid');
-
+    
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
-
+    
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-
-    var path = require('path');
+    
     var scriptName = path.dirname(path.dirname(path.dirname(__filename)));
-
+    
     sampleFile = req.files.sampleFile;
     let Sources = [];
     if (sampleFile.length > 1 && sampleFile) {
@@ -26,7 +40,7 @@ module.exports = {
           uploadPath = scriptName + '/data/img/' + val ;
           obj.mv(uploadPath, function (err) {
             if (err)
-              return res.status(500).send(err)
+            return res.status(500).send(err)
           });
           Sources.push(val)
         } else if (obj.mimetype.match(/video/g)) {
@@ -34,7 +48,7 @@ module.exports = {
           uploadPath = scriptName + '/data/video/' + val ;
           obj.mv(uploadPath, function (err) {
             if (err)
-              return res.status(500).send(err)
+            return res.status(500).send(err)
           });
           Sources.push(val)
         }
@@ -45,7 +59,7 @@ module.exports = {
         uploadPath = scriptName + '/data/img/' + val ;
         sampleFile.mv(uploadPath, function (err) {
           if (err)
-            return res.status(500).send(err)
+          return res.status(500).send(err)
         });
         Sources.push(val)
       } else if (sampleFile.mimetype.match(/video/g)){
@@ -53,12 +67,12 @@ module.exports = {
         uploadPath = scriptName + '/data/video/' + val ;
         sampleFile.mv(uploadPath, function (err) {
           if (err)
-            return res.status(500).send(err)
+          return res.status(500).send(err)
         });
         Sources.push(val)
       }
     }
-
+    
     res.send(Sources)
   }
 }
--- app/routes/upload.js
+++ app/routes/upload.js
@@ -4,6 +4,8 @@ const upload = require('../controllers/upload.js')
 router
     .route('/')
     .post(upload.POST)
+    .get(upload.GET)
+
 
 
 
