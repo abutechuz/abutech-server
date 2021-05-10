0cbda085e1ab619bdcb33bc3e63d8988e5974b8b
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2021-05-10 18:13:32 +0500

    chang file name "Library" to "library"

--- app/models/blog.js
+++ app/models/blog.js
@@ -1,8 +1,8 @@
 const { fetch, fetchOne } = require('../library/database/postgres')
 
-const getBlogs = async ({query : { page , limit , lang }}) => {
+const getBlogs = async ({ query: { page, limit, lang } }) => {
   lang = lang ?? 'uz'
-  const SQL = `select 
+  const SQL = `select
   blog_id ,
   blog_body_${lang} blog_body,
   blog_created_at
@@ -15,20 +15,20 @@ const getBlogs = async ({query : { page , limit , lang }}) => {
   return blogs
 }
 
-const insertBlog = async ({body : { blog_body_uz , blog_body_ru , blog_body_en }}) => {
-  
+const insertBlog = async ({ body: { blog_body_uz, blog_body_ru, blog_body_en } }) => {
+
   const SQL = `insert into blogs (blog_body_uz , blog_body_ru , blog_body_en ) values ($1) returning *`
-  
-  const response = await fetchOne(SQL,  blog_body_uz , blog_body_ru , blog_body_en )
-  
+
+  const response = await fetchOne(SQL, blog_body_uz, blog_body_ru, blog_body_en)
+
   return response
 }
 
-const setBlog = async ({body : {  blog_body_uz , blog_body_ru , blog_body_en , blog_id }}) => {
-  
+const setBlog = async ({ body: { blog_body_uz, blog_body_ru, blog_body_en, blog_id } }) => {
+
   const SQL = `update blogs set  blog_body_uz=$1 , blog_body_ru=$2 , blog_body_en=$3 where blog_id=$4 returning *`
-  
-  const res = await fetchOne(SQL,  blog_body_uz , blog_body_ru , blog_body_en , blog_id )
+
+  const res = await fetchOne(SQL, blog_body_uz, blog_body_ru, blog_body_en, blog_id)
 
   return res
 }
