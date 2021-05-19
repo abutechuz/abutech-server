058109f5ade9eb65425dffaa29775c9fe845f10e
rasuloff789 <rasuloff789@gmail.com>
2021-05-19 10:02:24 +0500

    ok

--- app/models/faq.js
+++ app/models/faq.js
@@ -32,7 +32,6 @@ const addFaq = async ({ body: { faq_question_uz,
 
 const deleteFaq = async ({body : {faq_id}}) => {
   const SQL = `delete from faq where faq_id=$1`
-  
   return await fetch(SQL , faq_id)
 }
 
