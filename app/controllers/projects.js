33dc833ddb53dad178017a5421f38483adebd569
rasuloff789 <rasuloff789@gmail.com>
2021-05-20 10:27:49 +0500

    project ok

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,5 +1,9 @@
 const projectsModel = require('../models/projects.js')
-const { verify } = require('../library/jwt.js')
+const {
+  verify
+} = require('../library/jwt.js')
+const path = require("path")
+const scriptName = path.dirname(path.dirname(path.dirname(__filename)))
 
 module.exports = {
   GET: async (req, res) => {
@@ -15,9 +19,52 @@ module.exports = {
 
   POST: async (req, res) => {
     try {
-      const blogs = await projectsModel.insertProject(req)
+      const def_obj = {
+        mimetype: ""
+      }
+      const project_img = req.files.project_img || def_obj;
+      const project_old_img = req.files.project_old_img || def_obj;
+      const project_new_img = req.files.project_new_img || def_obj;
 
-      res.send(blogs)
+      if (!req.files || Object.keys(req.files).length < 3 || !project_img.mimetype.match(/image/g) || !project_old_img.mimetype.match(/image/g) || !project_new_img.mimetype.match(/image/g)) {
+        return res.status(400).send({
+          message: 'File ERROR'
+        });
+      }
+
+      const project_img_uuid = require("uuid").v4();
+      const project_old_img_uuid = require("uuid").v4();
+      const project_new_img_uuid = require("uuid").v4();
+      const uploadPath = scriptName + '/data/img/';
+
+      if (project_img.mimetype.match(/image/g)) {
+        project_img.mv(uploadPath + project_img_uuid, async function (err) {
+          if (err) {
+            return res.status(500).send(err);
+          } else {
+            if (project_old_img.mimetype.match(/image/g)) {
+              project_old_img.mv(uploadPath + project_old_img_uuid,
+                async function (erro) {
+                  if (erro) {
+                    return res.status(500).send(err);
+                  } else {
+                    if (project_new_img.mimetype.match(/image/g)) {
+                      project_new_img.mv(uploadPath + project_new_img_uuid,
+                        async function (error) {
+                          if (error) {
+                            return res.status(500).send(err);
+                          } else {
+                            const project = await projectsModel.insertProject(req, project_img_uuid, project_old_img_uuid, project_new_img_uuid)
+                            return res.send(project)
+                          }
+                        })
+                    }
+                  }
+                })
+            }
+          }
+        });
+      }
     } catch (error) {
       res.send(error)
     }
--- app/models/projects.js
+++ app/models/projects.js
@@ -9,7 +9,7 @@ const getProjects = async ({
     limit
   }
 }) => {
-  
+
   const SQL = `select 
   p.project_id ,
   p.project_image ,
@@ -61,58 +61,56 @@ const getProjects = async ({
   where p.project_visible=true
   order by p.project_id desc
   offset ($1 - 1) * $2 fetch next $2 rows only ;`
-  
+
   return await fetch(SQL, page, limit)
 }
 
 const insertProject = async ({
-  body: {
-    project_image ,
-    project_type,
-    project_techs ,
-    project_client ,
-    project_team ,
-    project_platforms ,
-    project_industry ,
-    project_time ,
-    project_release ,
-    
-    project_title_uz ,
-    project_title_ru ,
-    project_title_en ,
-    
-    project_subtitle_uz ,
-    project_subtitle_ru ,
-    project_subtitle_en ,
-    
-    project_overview_uz ,
-    project_overview_ru ,
-    project_overview_en ,
-    
-    project_idea_uz ,
-    project_idea_ru ,
-    project_idea_en ,
-    
-    project_audience_uz ,
-    project_audience_ru ,
-    project_audience_en ,
-    
-    project_challenges_uz ,
-    project_challenges_ru ,
-    project_challenges_en ,
-    
-    project_result_uz ,
-    project_result_ru ,
-    project_result_en ,
-    
-    project_gplay_link ,
-    project_appstore_link ,
-    project_web_link ,
-    project_old_img_src ,
-    project_new_img_src 
-  }
-}) => {
-  
+    body: {
+      project_type,
+      project_techs,
+      project_client,
+      project_team,
+      project_platforms,
+      project_industry,
+      project_time,
+      project_release,
+
+      project_title_uz,
+      project_title_ru,
+      project_title_en,
+
+      project_subtitle_uz,
+      project_subtitle_ru,
+      project_subtitle_en,
+
+      project_overview_uz,
+      project_overview_ru,
+      project_overview_en,
+
+      project_idea_uz,
+      project_idea_ru,
+      project_idea_en,
+
+      project_audience_uz,
+      project_audience_ru,
+      project_audience_en,
+
+      project_challenges_uz,
+      project_challenges_ru,
+      project_challenges_en,
+
+      project_result_uz,
+      project_result_ru,
+      project_result_en,
+
+      project_gplay_link,
+      project_appstore_link,
+      project_web_link
+    }
+  }, project_image, project_old_img_src,
+  project_new_img_src) => {
+
   const SQL = `insert into projects (
     project_image ,
     project_type,
@@ -150,52 +148,52 @@ const insertProject = async ({
     project_old_img_src ,
     project_new_img_src 
     ) values ($1, $2 , $3 , $4 , $5 , $6 , $7 ,$8 ,$9 , $10 ,$11 , $12 ,$13 ,$14 , $15 , $16 , $17 , $18 ,$19 , $20 , $21 , $22 ,$23 , $24 , $25 , $26 , $27 ,$28 ,$29 , $30 , $31, $32 , $33 , $34 , $35 ) returning *`
-  
-  return await fetchOne(SQL, 
-    project_image ,
+
+  return await fetchOne(SQL,
+    project_image,
     project_type,
-    project_techs ,
-    project_client ,
-    project_team ,
-    project_platforms ,
-    project_industry ,
-    project_time ,
-    project_release ,
-    
-    project_title_uz ,
-    project_title_ru ,
-    project_title_en ,
-    
-    project_subtitle_uz ,
-    project_subtitle_ru ,
-    project_subtitle_en ,
-    
-    project_overview_uz ,
-    project_overview_ru ,
-    project_overview_en ,
-    
-    project_idea_uz ,
-    project_idea_ru ,
-    project_idea_en ,
-    
-    project_audience_uz ,
-    project_audience_ru ,
-    project_audience_en ,
-    
-    project_challenges_uz ,
-    project_challenges_ru ,
-    project_challenges_en ,
-    
-    project_result_uz ,
-    project_result_ru ,
-    project_result_en ,
-    
-    project_gplay_link ,
-    project_appstore_link ,
-    project_web_link ,
-    project_old_img_src ,
-    project_new_img_src 
-    )
+    JSON.parse(project_techs),
+    project_client,
+    project_team,
+    project_platforms,
+    project_industry,
+    project_time,
+    project_release,
+
+    project_title_uz,
+    project_title_ru,
+    project_title_en,
+
+    project_subtitle_uz,
+    project_subtitle_ru,
+    project_subtitle_en,
+
+    project_overview_uz,
+    project_overview_ru,
+    project_overview_en,
+
+    project_idea_uz,
+    project_idea_ru,
+    project_idea_en,
+
+    project_audience_uz,
+    project_audience_ru,
+    project_audience_en,
+
+    JSON.parse(project_challenges_uz),
+    JSON.parse(project_challenges_ru),
+    JSON.parse(project_challenges_en),
+
+    project_result_uz,
+    project_result_ru,
+    project_result_en,
+
+    project_gplay_link,
+    project_appstore_link,
+    project_web_link,
+    project_old_img_src,
+    project_new_img_src
+  )
 }
 
 const deleteProject = async ({
@@ -204,9 +202,9 @@ const deleteProject = async ({
   }
 }) => {
   const SQL = `update projects set project_visible=false where project_id=$1 returning *`
-  
+
   return await fetchOne(SQL, project_id)
-  
+
 }
 
 
